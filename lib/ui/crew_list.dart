4527e02627763b29bcac9567e7cf94c8884819f2
berkay079 <94804286+berkay079@users.noreply.github.com>
2022-07-22 12:02:38 +0300

    crew_item, crew_detail and drawer menu added

--- lib/ui/crew_list.dart
+++ lib/ui/crew_list.dart
@@ -1,10 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_project_crew_marine/model/crew.dart';
+import 'package:flutter_test_project_crew_marine/data/strings.dart';
+import 'package:flutter_test_project_crew_marine/ui/crew_item.dart';
+import 'package:flutter_test_project_crew_marine/ui/drawer_menu.dart';
 
-class name extends StatelessWidget {
-  const name({Key? key}) : super(key: key);
+
+
+class CrewList extends StatelessWidget {
+  late final List<Crew>allCrew;
+
+  CrewList(){
+    allCrew = veriKaynaginiHazirla();
+    print(allCrew);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+   return Scaffold(
+      drawer: const DrawerMenu(),
+      appBar: AppBar(
+        title: Text('Crew'),
+      ),
+      body: Center(
+        child: ListView.builder(itemBuilder: (context, index){
+          return CrewItem(listCrew: allCrew[index]);
+        },
+        itemCount: allCrew.length,),
+      ),
+    );
+  }
+ 
+
+  List<Crew> veriKaynaginiHazirla() {
+    List<Crew> gecici = [];
+    for(int i=0; i<12; i++){
+      var crewName =Strings.First_Name[i];
+      var crewLastName =Strings.Last_Name[i];
+      var crewNationality =Strings.Nationality[i];
+      var crewTitle =Strings.Title[i];
+
+      Crew eklenecek = Crew(crewName,crewLastName,crewNationality,crewTitle);
+      gecici.add(eklenecek);
+    }
+    return gecici;
+  }
+
+
+
+    Theme bottomNavMenu() {
+    return Theme(
+      data: ThemeData(
+          canvasColor: Colors.cyan.shade100, primaryColor: Colors.orangeAccent),
+      child: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people_alt),
+              label: 'Crew',
+              backgroundColor: Colors.amber),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.map),
+              label: 'Marine',
+              backgroundColor: Colors.red),
+       
+        ],
+       
+      ),
+    );
   }
 }
